40528aec37190c308fab308f4a1c7423144f5201
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-21 23:36:53 -0500

    Initialized private field 'data' and wrote the 'getter' and 'setter' method in 'Matrix' class.

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,2 +1,21 @@
 public class Matrix {
+
+    //Field to hold the 2D array of integers
+    private int[][] data;
+
+    //Getter method for the data field
+    public int[][] getData(){
+        return data;
+    }
+
+    //Setter method with input validation
+    public void setData(int[][] data){
+
+        // Checks if data is empty and throws exception
+        if(data == null || data.length == 0 || data[0].length == 0){
+            throw new IllegalArgumentException("Data must have at least 1 row and 1 column.");
+
+        }
+        this.data = data;
+    }
 }
