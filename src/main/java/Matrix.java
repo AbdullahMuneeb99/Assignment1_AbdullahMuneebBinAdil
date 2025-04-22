716fb1981cc37052044c1bd5b9ddccb79facd5a0
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-21 23:48:30 -0500

    Added Constructors for the Matrix class

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -28,4 +28,25 @@ public class Matrix {
     public int columnCount(){
         return data[0].length;
     }
+
+    //Constructor that creates an empty matrix with given rows and columns as arguments
+    public Matrix(int rows, int cols){
+
+        // Checks if rows or columns are 0; if so, throws exception
+        if(rows < 1 || cols < 1){
+            throw new IllegalArgumentException("Matrix must have at least 1 row and 1 column");
+        }
+
+        this.data = new int[rows][cols];
+    }
+
+    //Constructor that uses an existing 2D array as the matrix
+    public Matrix(int[][] data){
+
+        // Checks if rows or columns are 0; if so, throws exception
+        if (data == null || data.length == 0 || data[0].length == 0){
+            throw new IllegalArgumentException("The array must have at least 1 row and 1 column");
+        }
+        this.data = data;
+    }
 }
