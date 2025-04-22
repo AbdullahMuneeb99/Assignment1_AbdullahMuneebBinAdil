3c085f00e072b55c2856d2e041a41c443b64ede0
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-22 00:08:38 -0500

    Created tests for Invalid Constructors (rows and columns)

--- src/test/java/MatrixTest.java
+++ src/test/java/MatrixTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+
 public class MatrixTest {
+
+    // Test Constructor if it throws exception for invalid rows
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidConstructorWithRows(){
+        new Matrix(0, 4);
+    }
+
+    // Test Constructor if it throws exception for invalid columns
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidConstructorWithCols(){
+        new Matrix(3, 0);
+    }
+
+
 }
