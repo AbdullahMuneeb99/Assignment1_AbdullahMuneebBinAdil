da4bcdd89760a6ef7080c4d942387296b7db7af0
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-24 22:27:00 -0500

    Extended MatrixOutlinePrinter with BoolMatrixPrinter.
    Implemented the protected String method printRow for binary matrices.

--- src/main/java/BoolMatrixPrinter.java
+++ src/main/java/BoolMatrixPrinter.java
@@ -1,2 +1,28 @@
-public class BoolMatrixPrinter {
+public class BoolMatrixPrinter extends MatrixOutlinePrinter{
+
+    // Overriding the printRow method to handle 0s and 1s
+    @Override
+    protected String printRow(Matrix matrix, int rowIndex){
+
+        int cols = matrix.columnCount(); // gets the number of columns
+        StringBuilder row = new StringBuilder();
+
+        for(int col = 0; col < cols; col++) {
+            int value = matrix.getData()[rowIndex][col];
+
+
+            // Check if value is 0 or 1
+            if (value != 0 && value != 1) {
+                throw new IllegalArgumentException("Matrix must contain only 0s and 1s.");
+            }
+
+            //Appending space for 0s and 1s according to the sample in question
+            if (value == 0) {
+                row.append(" ");
+            } else {
+                row.append("#");
+            }
+        }
+        return row.toString();
+    }
 }
