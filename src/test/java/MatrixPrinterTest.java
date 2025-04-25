d1b5dd43de64b83a617f60d6adbf279a87a0a258
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-24 22:55:35 -0500

    Implemented the "IllegalArgumentException" test for BoolMatrixPrinter class.

--- src/test/java/MatrixPrinterTest.java
+++ src/test/java/MatrixPrinterTest.java
@@ -16,4 +16,16 @@ public class MatrixPrinterTest {
 
         assertEquals(expected, printer.print(matrix));
     }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testBoolMatrixPrinterInvalidMatrix(){
+        Matrix matrix = new Matrix(2, 3);
+
+        int[][] data = {{0, 1, 2}, {1, 0, 1}}; // Invalid input because 2 is not allowed
+
+        matrix.setData(data); // calls the setter method
+
+        MatrixPrinter printer = new BoolMatrixPrinter();
+        printer.print(matrix); // Should result in exception
+    }
 }
