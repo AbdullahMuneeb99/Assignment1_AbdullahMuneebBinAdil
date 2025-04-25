0fafc84c371d80cfdd7421a27f2550edaf85fa11
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-24 22:49:08 -0500

    Implemented the test for MatrixOutlinePrinter. Tests empty Matrix and checks if the size and string match the expected output.

--- src/test/java/MatrixPrinterTest.java
+++ src/test/java/MatrixPrinterTest.java
@@ -1,2 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class MatrixPrinterTest {
+
+
+    @Test
+    public void testOutlineMatrixPrinterSize() {
+        Matrix matrix = new Matrix(3, 4); // Creates empty matrix of size 3X4
+        MatrixPrinter printer = new MatrixOutlinePrinter();
+
+
+        // expected result
+        String expected =
+                "+----+\n" + "|    |\n" + "|    |\n" + "|    |\n" + "+----+";
+
+        assertEquals(expected, printer.print(matrix));
+    }
 }
