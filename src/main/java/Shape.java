a587fea934dbb1f7b60d63377fb1b108c6ee9046
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-24 23:46:39 -0500

    Implemented the Constructor for the Shape class.

--- src/main/java/Shape.java
+++ src/main/java/Shape.java
@@ -1,2 +1,25 @@
-public class Shape {
+public class Shape extends Matrix{
+
+    private String name;
+
+    // Constructor with name and data array (data should be in binary)
+    public Shape(String name, int[][] data) {
+        super(data); // Call Matrix constructor to set data
+
+        // Validating name of shape
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Shape must have a name.");
+        }
+
+        // Checking that data only contains 0s and 1s
+        for (int[] row : data) {
+            for (int value : row) {
+                if (value != 0 && value != 1) {
+                    throw new IllegalArgumentException("Shape matrix must contain only 0s and 1s.");
+                }
+            }
+        }
+
+        this.name = name;
+    }
 }
