ea36b78215b0436350db1a04d2e564ae50b18d7a
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-25 00:24:14 -0500

    Starting the "GameOfLife" class with creating a constructor.

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,2 +1,11 @@
-public class GameOfLife {
+public class GameOfLife extends Matrix{
+
+    // Constructor: creates a game field with specified size
+    public GameOfLife(int rows, int cols) {
+
+        super(rows, cols); // Call Matrix constructor
+
+    }
+
+
 }
