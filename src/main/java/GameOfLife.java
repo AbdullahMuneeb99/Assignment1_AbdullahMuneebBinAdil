35206121be25737e5915d9b01ab508615673f14d
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-25 00:32:49 -0500

    Created the 'addShape' function in "GameOfLife.java". It validates if the shape fits on the board, then creates a matrix board for it.

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -7,5 +7,30 @@ public class GameOfLife extends Matrix{
 
     }
 
+    // Adds the required shape at a given position (row, col)
+    public void addShape(Shape shape, int startRow, int startCol) {
+
+        // Gets the data (values in Matrix)
+        int[][] shapeData = shape.getData();
+
+
+        int shapeRows = shapeData.length;
+        int shapeCols = shapeData[0].length;
+
+        // Validating that the shape fits on the board
+        if (startRow < 0 || startCol < 0 || startRow + shapeRows > this.rowCount() || startCol + shapeCols > this.columnCount()) {
+            throw new IllegalArgumentException("Shape does not fit at the given position.");
+        }
+
+        // Copies data and sets it on the Matrix
+        int[][] newData = getData(); // Copy current data
+        for (int i = 0; i < shapeRows; i++) {
+            for (int j = 0; j < shapeCols; j++) {
+                newData[startRow + i][startCol + j] = shapeData[i][j];
+            }
+        }
+
+        setData(newData); // Update board
+    }
 
 }
