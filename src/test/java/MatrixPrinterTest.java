fd8896665014774137fdba8f567cea337ff6a1ae
Abdullah Muneeb <abdullahmuneeb099@outlook.com>
2025-04-24 23:19:42 -0500

    Implemented the valid input tests in MatrixPrinterTest class.

--- src/test/java/MatrixPrinterTest.java
+++ src/test/java/MatrixPrinterTest.java
@@ -17,6 +17,7 @@ public class MatrixPrinterTest {
         assertEquals(expected, printer.print(matrix));
     }
 
+    // Tests invalid matrix input (not all values are 0 or 1)
     @Test (expected = IllegalArgumentException.class)
     public void testBoolMatrixPrinterInvalidMatrix(){
         Matrix matrix = new Matrix(2, 3);
@@ -27,5 +28,34 @@ public class MatrixPrinterTest {
 
         MatrixPrinter printer = new BoolMatrixPrinter();
         printer.print(matrix); // Should result in exception
+    }
+
+    // Tests valid input (all values are either 0 or 1)
+    @Test
+    public void testBoolMatrixPrinterValidMatrix(){
+
+        // First matrix
+        Matrix matrix1 = new Matrix(2, 4);
+        matrix1.setData(new int[][] {
+                {0, 1, 0, 1},
+                {1, 0, 0, 1}
+        });
+
+        String expected1 =
+                "+----+\n" + "| # #|\n" + "|#  #|\n" + "+----+";
+
+        assertEquals(expected1, new BoolMatrixPrinter().print(matrix1));
+
+        // Second matrix
+        Matrix matrix2 = new Matrix(1, 3);
+        matrix2.setData(new int[][] {
+                {1, 0, 1}
+        });
+
+        String expected2 =
+                "+---+\n" + "|# #|\n" + "+---+";
+
+        assertEquals(expected2, new BoolMatrixPrinter().print(matrix2));
+
     }
 }
